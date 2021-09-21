265a7d7791863e9cb9832fe53447d17fca3e1aa3
Abdullah Alrayes <abdullah.a.abas@gmail.com>
2021-09-21 11:09:27 +0300

    fsinihed

--- calculator.js
+++ calculator.js
@@ -1,6 +1,65 @@
-const prompt = require("prompt-sync")(); // Please don't touch me :)
+const prompt = require('prompt-sync')(); // Please don't touch me :)
 
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+const validateNumber = (value) => {
+  if (!isNaN(value) && typeof value === 'number') return true;
+  else {
+    console.log('Invalid input, please add a number');
+    return false;
+  }
+};
+const validateOperator = (value) => {
+  if (
+    typeof value === 'string' &&
+    value !== '+' &&
+    value !== '-' &&
+    value !== '/' &&
+    value !== '*'
+  ) {
+    console.log('Invalid inputs, please add a valid operator');
+    return false;
+  }
+  return true;
+};
+const validateContinue = (value) => {
+  if (value === 'No' || value === 'N' || value === 'no' || value === 'n')
+    return false;
+  else {
+    if (value === 'Yes' || value === 'Y' || value === 'yes' || value === 'y')
+      return true;
+    else {
+      return null;
+    }
+  }
+};
+const getResult = (first, second, operator) => {
+  if (operator === '+') {
+    return first + second;
+  } else if (operator === '-') {
+    return first - second;
+  } else if (operator === '/') {
+    return first / second;
+  } else if (operator === '*') {
+    return first * second;
+  } else {
+    return 'Error!';
+  }
+};
+
+let first_number = prompt('Please enter the first number: ');
+first_number = parseInt(first_number);
+const isFirstValid = validateNumber(first_number);
+if (isFirstValid) {
+  let second_number = prompt('Please enter the second number: ');
+  second_number = parseInt(second_number);
+  const isSecondValid = validateNumber(second_number);
+  if (isSecondValid) {
+    const operator = prompt('Please choose the operation (+, -, /, *): ');
+    const isValidOperator = validateOperator(operator);
+    if (isValidOperator) {
+      const result = getResult(first_number, second_number, operator);
+      console.log('result is: ' + result);
+    }
+  }
+}
